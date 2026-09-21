69d1a4775f3c5bfa28b8af6eb9da2054a0ffacf6
ElishaMelAltairNarisma <aritiruerisha@gmail.com>
2026-09-21 20:29:33 +0800

    Refactor calculatePay logic - narisma.elishamelaltair

--- shifts.js
+++ shifts.js
@@ -4,8 +4,9 @@ function isValidShift(hours) {
 
 function calculatePay(hours, rate) {
   if (hours > 8) {
-    const regularPay = 8 * rate;
-    const overtimeHours = hours - 8;
+    const regularHours = 8;
+    const regularPay = regularHours * rate;
+    const overtimeHours = hours - regularHours;
     const overtimePay = overtimeHours * rate * 1.5;
     return Math.round(regularPay + overtimePay);
   }
